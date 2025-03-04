fd80a42f1f51c94e8e9728740fff4793c36db7eb
persexgithub <79389819+persexgithub@users.noreply.github.com>
2025-03-04 09:34:02 -0500

    cleaning code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/blockhouse-assessment",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
